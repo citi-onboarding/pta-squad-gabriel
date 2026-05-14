b958a6128be1e353482406bf72b80f27f673567a
Eduardovdcn <72761294+Eduardovdcn@users.noreply.github.com>
2026-05-14 17:52:28 -0300

    feat/CRUD completed

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import userController from "./controllers/UserController";
+import livroController from "./controllers/LivroController";
 
 const routes = express.Router();
 
@@ -7,5 +8,10 @@ routes.post("/user", userController.create);
 routes.get("/user", userController.get);
 routes.delete("/user/:id", userController.delete);
 routes.patch("/user/:id", userController.update);
+
+routes.post("/livros", livroController.create);
+routes.get("/livros", livroController.get);
+routes.get("/livros/:id", livroController.getById);
+routes.delete("/livros/:id", livroController.delete);
 
 export default routes;
