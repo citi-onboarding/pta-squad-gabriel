e3dc4a843c2796ef768dc2dd611c8036b197ec8a
joaoazevedocavalcanti <joaoazevedocavalcanti@gmail.com>
2026-05-15 11:53:23 -0300

    uptade-page

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,9 +1,20 @@
-import CadastrarLivro from "@/components/CadastrarLivro";
+import Image from "next/image";
+
+import { LogoCITi } from "../assets";
 
 export default function Home() {
   return (
-    <div>
-      <CadastrarLivro />
+    <div className="flex flex-1 flex-col h-full justify-around items-center bg-black">
+      <div>
+        <Image src={LogoCITi} alt="Logo citi" />
+      </div>
+      <div className="flex flex-col justify-center items-center">
+        <h1 className="text-white text-4xl font-bold">NextJS Boilerplate</h1>
+        <p className="text-white text-xl">
+          Made with <strong>&lt; &#x0002F; &gt;</strong> and{" "}
+          <strong>&hearts;</strong> by CITi
+        </p>
+      </div>
     </div>
   );
 }
