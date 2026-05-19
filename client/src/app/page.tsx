a0ed2c5a1f01505173a1b21bdc0e7597fb66ba19
Eduardovdcn <72761294+Eduardovdcn@users.noreply.github.com>
2026-05-18 23:48:31 -0300

    feat: modal working - todo: style

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,17 +1,34 @@
-import Image from "next/image";
+'use client';
 
-import { LogoCITi } from "../assets";
+import Image from 'next/image';
+import { LogoCITi } from '../assets';
+import { Emprestimo } from '../components/modal/emprestimo';
+import type { EmprestDataProps } from '../components/modal/emprestimo';
+import CadastrarLivro from '@/components/CadastrarLivro';
 
 export default function Home() {
+  const livroMock = {
+    id: 1,
+    titulo: 'O Guia do Mochileiro das Galáxias',
+  };
+
+  function handleConfirm(dados: EmprestDataProps) {
+    console.log('Empréstimo confirmado:', dados);
+  }
+
   return (
     <div className="flex flex-1 flex-col h-full justify-around items-center bg-black">
       <div>
         <Image src={LogoCITi} alt="Logo citi" />
       </div>
-      <div className="flex flex-col justify-center items-center">
-        <h1 className="text-white text-4xl font-bold">NextJS Boilerplate</h1>
+      <div className="flex flex-col justify-center items-center gap-4">
+        <CadastrarLivro></CadastrarLivro>
+        <Emprestimo
+          livro={livroMock}
+          onConfirm={handleConfirm}
+        />
         <p className="text-white text-xl">
-          Made with <strong>&lt; &#x0002F; &gt;</strong> and{" "}
+          Made with <strong>&lt; &#x0002F; &gt;</strong> and{' '}
           <strong>&hearts;</strong> by CITi
         </p>
       </div>
