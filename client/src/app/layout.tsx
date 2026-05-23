7e569f506e1e58b370d081edb0b7b358725cb97f
Eduardovdcn <72761294+Eduardovdcn@users.noreply.github.com>
2026-05-23 20:53:43 -0300

    fix: toast in success and error

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import Header from "@/components/header";
 import "styles/globals.css";
+import { Toaster } from "sonner";
 
 export const metadata: Metadata = {
   title: "Biblioteca Escolar",
@@ -18,6 +19,7 @@ export default function RootLayout({
       <body>
         <Header />
         {children}
+        <Toaster></Toaster>
       </body>
     </html>
   );
--- client/src/app/livros/novo/page.tsx
+++ client/src/app/livros/novo/page.tsx
@@ -3,12 +3,18 @@ import { useState } from "react";
 import CadastrarLivro from "@/components/CadastrarLivro";
 import { useRouter } from "next/navigation";
 import api from "@/lib/api";
+import { toast, Toaster } from "sonner";
 
 export default function NovoLivro() {
   const router = useRouter()
 
   async function handleCadastrarLivro(livro: any) {
-    await api.post("/livros", livro)
+    try {
+      await api.post("/livros", livro)
+      toast.success('Cadastrado com sucesso')
+    } catch (error: any) {
+      toast.error(error.message || "Tente novamente.");    
+}
 
     router.push("/livros");
   }
