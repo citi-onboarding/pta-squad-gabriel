fb414fe15f500874accd59535ce9963231090786
evdcn <evdcn@g1c42.cin.ufpe.br>
2026-05-28 13:59:48 -0300

    fix(integrate-devolucao): update books after getLivroPorId

---
+++ client/src/services/emprestimoService.ts
@@ -0,0 +1,20 @@
+import { Emprestimo } from "@/types";
+import api from "../lib/api";
+
+export async function createEmprestimo(id: string) {
+    try {
+        const response = await api.post(`/emprestimos/${id}`);
+        return response.data;
+} catch (error) {
+    console.error("Erro ao criar empréstimo:", error);       
+     }
+}
+
+export async function devolverEmprestimo(id: string) {
+  try {
+    const response = await api.patch(`/emprestimos/${id}/devolver`);
+    return response.data;
+  } catch (error) {
+    console.error("Erro ao devolver empréstimo:", error);
+   }
+}
