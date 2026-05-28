47083ac307a9ee8a843e968e335669244a1b2772
Matheus Lins <thecolins08@gmail.com>
2026-05-28 00:11:14 -0300

    feat/ create header for mobile aplication

--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -1,10 +1,9 @@
 import { View, Text } from "react-native";
+import HeaderMobile from "./components/Header";
 
 const App: React.FC = () => (
-  <View className="flex-1 justify-center items-center bg-gray-100">
-    <Text className="text-xl font-barlowBold text-[#58cbfb]">
-      Made with &lt; / &gt; and 🩵 by CITi
-    </Text>
+  <View >
+    <HeaderMobile />
   </View>
 );
 
