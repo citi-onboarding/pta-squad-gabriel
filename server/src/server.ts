aec7bf6f6c05a2fc531514c462f50c529541eaaf
Gabriel Ribeiro <gabrielcarleial@gmail.com>
2026-06-01 02:38:42 -0300

    chore: deploy settings

--- server/src/server.ts
+++ server/src/server.ts
@@ -24,6 +24,6 @@ app.use(express.json());
 app.use(routes);
 app.use(express.static(__dirname + "/public"));
 
-app.listen(process.env.SERVER_PORT || 3001, () => {
+app.listen(process.env.PORT || process.env.SERVER_PORT || 3001, () => {
   console.log("📦 Server running");
 });
