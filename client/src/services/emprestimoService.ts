5049820e22227d279ac8550bb3e5d996f9d84cc0
Gabriel Ribeiro <gabrielcarleial@gmail.com>
2026-06-01 00:32:34 -0300

    fix: update emprestimoService

--- client/src/services/emprestimoService.ts
+++ client/src/services/emprestimoService.ts
@@ -1,19 +1,6 @@
 import api from "../lib/api";
 
-export async function createEmprestimo(id: string) {
-    try {
-        const response = await api.post(`/emprestimos/${id}`);
-        return response.data;
-} catch (error) {
-    console.error("Erro ao criar empréstimo:", error);       
-     }
-}
-
 export async function devolverEmprestimo(id: string) {
-  try {
-    const response = await api.patch(`/emprestimos/${id}/devolver`);
-    return response;
-  } catch (error) {
-    console.error("Erro ao devolver empréstimo:", error);
-   }
+  const response = await api.patch(`/emprestimos/${id}/devolver`);
+  return response.data;
 }
