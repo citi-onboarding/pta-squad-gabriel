029c154488542642554fa8ec5606ffb1d81c9c49
Gabriel Ribeiro <gabrielcarleial@gmail.com>
2026-06-04 15:27:21 -0300

    fix: update zod error

--- server/src/DTOs/livroDTO.ts
+++ server/src/DTOs/livroDTO.ts
@@ -5,8 +5,8 @@ export const createLivroSchema = z.object({
   autor: z.string().min(1, "Autor é obrigatório."),
   isbn: z.string().regex(/^(?:\d{10}|\d{13})$/, "ISBN inválido."),
   editora: z.string().min(1, "Editora é obrigatória."),
-  ano: z.number({ required_error: "Ano é obrigatório." }),
-  quantidade_total: z.number({ required_error: "Quantidade total é obrigatória." }),
+  ano: z.number({ error: "Ano é obrigatório." }),
+  quantidade_total: z.number({ error: "Quantidade total é obrigatória." }),
   categoria: z.string().min(1, "Categoria é obrigatória."),
   foto_url: z.string().url("URL da foto inválida.").or(z.literal("")).optional(),
 });
